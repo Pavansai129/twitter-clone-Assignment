46774abd3c1396a6bad9a2ed02dca7e606655f42
pavansai <pavansaivvit@gmail.com>
2023-05-30 10:17:46 +0000

    API 2 is completed

--- app.js
+++ app.js
@@ -35,13 +35,11 @@ initializeDbAndServer();
 //API 1
 app.post("/register/", async (request, response) => {
   const { username, password, name, gender } = request.body;
-  console.log(username, password, name, gender);
   const selectUserQuery = `
     SELECT username FROM user WHERE username = "${username}";
     `;
   const isUserAlreadyExists = await db.get(selectUserQuery);
-  console.log(isUserAlreadyExists);
-  if (isUserAlreadyExists !== undefined) {
+  if (isUserAlreadyExists.username !== undefined) {
     response.status(400);
     response.send("User already exists");
   } else {
@@ -58,6 +56,31 @@ app.post("/register/", async (request, response) => {
     `;
       await db.run(queryToPostData);
       response.send("User created successfully");
+    }
+  }
+});
+
+//API 2
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `
+    SELECT * FROM user WHERE username = "${username}";
+    `;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
     }
   }
 });
